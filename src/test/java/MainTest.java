61be83b4f75064be2c9bb935740e96c0bb213711
Zdenko Rybčák <zdenkorybcak@gmail.com>
2022-03-26 01:40:11 +0100

    Add java maven projekt

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testSumOk(){
+
+        Main main = new Main();
+        main.sum(2,5);
+        assertEquals(7,main.sum(2,5));
+    }
+
+}
